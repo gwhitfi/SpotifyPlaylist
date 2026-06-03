6d5d7117e1fd0dc04d8e3249d36840e60f25149b
gwhitfi <justinwclemson@gmail.com>
2026-06-02 20:17:34 -0400

    fix: bug with URL Params

--- src/api/musicBrainzArtistSearch.ts
+++ src/api/musicBrainzArtistSearch.ts
@@ -16,17 +16,17 @@ interface MusicBrainzResponse {
 
 export async function musicBrainzArtistSearch(userInput: string): Promise<MusicBrainzArtist[]> {
     const params = new URLSearchParams({
-        artist: userInput,
+        query: userInput,
         fmt: "json",
     });
-
     try {
-        const response = await fetch(`https://musicbrainz.org/ws/2/artist?query=${params}`);
+        const response = await fetch(`https://musicbrainz.org/ws/2/artist?${params}`);
         if (!response.ok) {
             console.error(`MusicBrainz API Failed: ${response.status}`);
             return [];
         }
         const data: MusicBrainzResponse = await response.json();
+        console.log("DATA: ", data);
         return data.artists ?? [];
     } catch (error) {
         console.error(error);
