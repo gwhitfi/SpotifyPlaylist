54120ab2882072c0c50978d4dfcd379d79100d65
gwhitfi <justinwclemson@gmail.com>
2026-06-03 09:22:58 -0400

    fix: add typing, fix return values, token refresh handling

--- src/utils/getSpotifyProfile.ts
+++ src/utils/getSpotifyProfile.ts
@@ -1,18 +1,40 @@
 import { refreshToken } from "../api/spotifyAuth";
 
-export async function getSpotifyProfile(retried = false) {
-    const accessToken = localStorage.getItem("access_token");
-    const response = await fetch("https://api.spotify.com/v1/me", {
-        headers: {
-            Authorization: "Bearer " + accessToken,
-        },
-    });
+interface SpotifyProfile {
+    account_id: string;
+    country: string;
+    display_name: string;
+    email: string;
+    id: string;
+    uri: string;
+    images: {
+        height: number;
+        width: number;
+        url: string;
+    }[];
+}
 
-    if (response.status === 401 && !retried) {
-        const refreshed = await refreshToken();
-        if (!refreshed) return null;
-        return getSpotifyProfile(true);
+export async function getSpotifyProfile(): Promise<SpotifyProfile | null> {
+    await refreshToken();
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) return null;
+
+    try {
+        const response = await fetch("https://api.spotify.com/v1/me", {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        if (!response.ok) {
+            console.error(`Spotify API error on accessing user profile: ${response.status}`);
+            return null;
+        }
+
+        const data: SpotifyProfile = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return null;
     }
-    const data = await response.json();
-    return data;
 }
